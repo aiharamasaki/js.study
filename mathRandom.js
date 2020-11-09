b4a8b0b61b8339f80fbdbabe1c6c9f7076fcc2f5
hirasaki1985 <hirasaki1985@gmail.com>
2020-11-09 21:04:30 +0900

    getRandom()関数で文字の数字が与えられた時の挙動を修正、その他コメント・バグ修正

--- mathRandom.js
+++ mathRandom.js
@@ -1,14 +1,22 @@
+/**
+ * 引数の範囲内でランダムで出力した数値を返す
+ * @param {number} min
+ * @param {number} max
+ * @return {number|undefined}
+ */
 module.exports = function getRandom( min = 0 , max = 100 ) {
   console.log(min,max);
-  
-  if(isNaN(min,max)==true){
+
+  if(isNaN(min) || isNaN(max)){
     console.log('引数エラー：数値以外が入力されています');
+    return
   }
   else if(min>max){
     console.log('引数エラー：数値を入れ替えて入力してください');
+    return
   }
   else{
-  var random = Math.floor( Math.random() * (max + 1 - min) ) + min;
+  const random = Math.floor( Math.random() * (Number(max) + 1 - Number(min)) ) + Number(min);
   return random;
   }
 }
