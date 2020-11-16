42b6df9eb8add527589c92824cdc95df8487e0ab
aiharamasaki <aiharamasaki@kxa.biglobe.ne.jp>
2020-11-16 23:55:15 +0900

    number

--- mathRandom.js
+++ mathRandom.js
@@ -1,15 +1,21 @@
+/**
+ * 引数の範囲内でランダムで出力した数値を返す
+ * @param {number} min
+ * @param {number} max
+ * @return {number|undefined}
+ */
+
 module.exports = function getRandom( min = 0 , max = 100 ) {
-  
-  if(isNaN(min,max)==true){
+  if(isNaN(min) || isNaN(max)){
     console.log('引数エラー：数値以外が入力されています');
+    return
   }
   else if(min>max){
     console.log('引数エラー：数値を入れ替えて入力してください');
+    return
   }
   else{
-    maxNumber = Number(max);
-    minNumber = Number(min);
-    var random = Math.floor( Math.random() * (maxNumber + 1 - minNumber) ) + minNumber;
+    const random = Math.floor( Math.random() * (Number(max) + 1 - Number(min)) ) + Number(min);
   return random;
   }
 }
